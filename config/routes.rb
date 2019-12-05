1e2b8f6052137974b54f209a0f132125b46a07ce
ManojParitala <manojparitala@gmail.com>
2019-12-05 10:11:07 -0600

    Building a twitter like app using Ruby on Rails

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "dashboards#show"
+  end
+
+  get '/' => "homes#show"
+  resources :shouts, only: [:create]
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, only: [:create]
+
+  resources :users, only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
+  get "/sign_up" => "users#new", as: "sign_up"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
